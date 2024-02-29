c7a638f30c622f06f7688a4a1f8188e02f0c42a6
Almir <almiruwow@gmail.com>
2024-02-29 20:34:53 +0300

    Добавление фикстур

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,9 +1,20 @@
 from utils.dicts import get_val
+import pytest
 
 
-def test_get_val():
-    assert get_val({"vcs": "mercurial"}, "vcs") == 'mercurial'
-    assert get_val({}, "vcs", "git") == 'git'
-    assert get_val({"vcs": "mercurial"}, "vcs", "git") == 'mercurial'
-    assert get_val({}, "vcs", "bazaar") == 'bazaar'
-    assert get_val({}, "vcs") == 'git'
+@pytest.fixture
+def data_fixture():
+    return {"vcs1": "mercurial", "vcs2": "git", "vcs3": "bazaar"}
+
+
+@pytest.fixture
+def data_none_fixture():
+    return {}
+
+
+def test_get_val(data_fixture, data_none_fixture):
+    assert get_val(data_fixture, "vcs1") == 'mercurial'
+    assert get_val(data_none_fixture, "vcs", "git") == 'git'
+    assert get_val(data_fixture, "vcs3", "mercurial") == 'bazaar'
+    assert get_val(data_none_fixture, "vcs2", "bazaar") == 'bazaar'
+    assert get_val(data_none_fixture, "vcs1") == 'git'
